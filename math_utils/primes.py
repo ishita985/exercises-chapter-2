a00be1d06f7b2ea6da268cec9db55ada0c3b0446
Ishita Swaroop <isswaroop@gmail.com>
2024-10-17 13:44:10 +0100

    Made sure it does not say 1 is prime. Switched the "True" and "False" positions since I mixed them up.

--- math_utils/primes.py
+++ math_utils/primes.py
@@ -2,7 +2,10 @@ from math import isqrt
 
 def isprime(n):
     """Tells us whether an integer is prime"""
-    for i in range(1, isqrt(n)): #isqrt gives us the square root rounded down to the nearest integer
-        if n % i == 0:
-            return True
-    return False
+    if n == 1:
+        return False
+    else:
+        for i in range(2, isqrt(n)): #isqrt gives us the square root rounded down to the nearest integer
+            if n % i == 0:
+                return False
+        return True
